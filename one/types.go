338731804c5dcea2f31027811295411093ad5a92
Ross <rossmylesobrien@gmail.com>
2019-10-12 15:38:37 +0100

    Adding types

---
+++ one/types.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+var y = 42
+var x = "Shaken, not stirred"
+
+func main() {
+
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+}
