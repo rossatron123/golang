87887796e2cf81452be9281b48ef5a7f40e87933
Ross <rossmylesobrien@gmail.com>
2019-10-13 15:52:11 +0100

    Adding a multi-dimensional slice

---
+++ four/multi-dimensional_slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	jb := []string{"James","Bond","Chocolate","Martini"}
+	fmt.Println(jb)
+
+	mp := []string{"Miss","Moneypenny","Strawberry","Vodka"}
+	fmt.Println(mp)
+
+	xp := [][]string{jb,mp}
+	fmt.Println(xp)
+}
